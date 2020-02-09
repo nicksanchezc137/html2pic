8717b3da0a5cb4f128c727d463f4dc1e377cfe2a
koragenick <nick.korage@invent.co.ke>
2020-02-09 17:34:02 +0300

    make it better

---
+++ routes/pdf2pic.js
@@ -0,0 +1,27 @@
+var express = require("express");
+const puppeteer = require("puppeteer");
+var router = express.Router();
+
+
+/* POST users listing. */
+router.post("/", function(req, res) {
+  var html = req.body.html;
+  
+ convertImageToImage(html,res);
+});
+
+const convertImageToImage = async (html,res) => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 600,
+    height: 800,
+    deviceScaleFactor:1
+  });
+  await page.setContent(html);
+  const base64 =  await page.screenshot({ encoding: "base64" });
+ // console.log("Base 64 is ", base64);
+  await browser.close();
+  res.send( {base64:base64});
+};
+module.exports = router;
