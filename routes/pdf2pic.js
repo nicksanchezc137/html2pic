67de25afc9e97b748f11bfc254ea2650d8f65347
koragenick <nick.korage@invent.co.ke>
2020-02-09 18:02:55 +0300

    make it better

--- routes/pdf2pic.js
+++ routes/pdf2pic.js
@@ -11,7 +11,7 @@ router.post("/", function(req, res) {
 });
 
 const convertImageToImage = async (html,res) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
   await page.setViewport({
     width: 600,
