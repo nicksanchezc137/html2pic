81950721957cd529502694ddbfbf2f3f0df63e2a
koragenick <nick.korage@invent.co.ke>
2020-06-28 16:12:51 +0300

    logger

--- app.js
+++ app.js
@@ -8,14 +8,15 @@ var debug = require('debug')('express-api:server');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pdf2picRouter = require('./routes/pdf2pic');
-
+var winston = require('./config/winston');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+//app.use(logger('dev'));
+app.use(logger('combined', { stream: winston.stream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -28,6 +29,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pdf2pic', pdf2picRouter);
 
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -39,6 +42,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  winston.error(`${err.status || 500} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 
   // render the error page
   res.status(err.status || 500);
--- routes/pdf2pic.js
+++ routes/pdf2pic.js
@@ -1,33 +1,42 @@
 var express = require("express");
 const puppeteer = require("puppeteer");
+
+
 var router = express.Router();
 
-router.use(function(req, res, next) {
+router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
 /* POST users listing. */
-router.post("/", function(req, res) {
+router.post("/", function (req, res) {
   var html = req.body.html;
-  
- convertImageToImage(html,res);
+
+  convertImageToImage(html, res);
 });
 
-const convertImageToImage = async (html,res) => {
-  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});//production linux server
-  //const browser = await puppeteer.launch(); //development
-  const page = await browser.newPage();
-  await page.setViewport({
-    width: 600,
-    height: 800,
-    deviceScaleFactor:1
-  });
-  await page.setContent(html);
-  const base64 =  await page.screenshot({ encoding: "base64" });
- // console.log("Base 64 is ", base64);
-  await browser.close();
-  res.send( {base64:base64});
+const convertImageToImage = async (html, res) => {
+ 
+    const browser = await puppeteer.launch({
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    }); //production linux server
+    //const browser = await puppeteer.launch(); //development
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 600,
+      height: 800,
+      deviceScaleFactor: 1,
+    });
+    await page.setContent(html);
+    const base64 = await page.screenshot({ encoding: "base64" });
+    // console.log("Base 64 is ", base64);
+    await browser.close();
+    res.send({ base64: base64,message:"success" });
+  
 };
 module.exports = router;
